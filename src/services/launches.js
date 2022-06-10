d876d2a063f3b3207094e418d9e419eebd2f11d8
Artikunazo <artik16032011@gmail.com>
2022-06-10 03:36:33 -0700

    1 file modified

--- src/services/launches.js
+++ src/services/launches.js
@@ -1,5 +1,11 @@
+import config from '../config/config';
+
 export async function getAllLaunches () {
   try {
-    const response = await fetch()
+    const response = await fetch(config.api);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
   }
 }
