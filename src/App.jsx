1e9467472dd1ef6bb79f69c1c47113ac763923c3
Artikunazo <artik16032011@gmail.com>
2022-06-12 01:46:07 -0700

    1 file modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,27 @@
-import './card.css';
+import React from 'react';
+import { useState, useEffect } from 'react';
+import './style.css';
+import * as API from './services/launches';
 
-class Card
+export default function App() {
+    const [launches, setLaunches] = useState([]);
+
+    useEffect(() => {
+      API.getAllLaunches().then(setLaunches)
+    }, []);
+
+    return (
+          <>
+                <h1>SpaceX Launches</h1>
+                      <section>
+                              <ul>
+                                        {launches?.map((launch) => (
+                                                    <li key={launch.flight_number}>
+                                                                  {launch.mission_name} ({launch.launch_year})
+                                                                              </li>
+                                                                                        ))}
+                                                                                                </ul>
+                                                                                                      </section>
+                                                                                                          </>
+                                                                                                            );
+}
