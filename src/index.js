e8144fe5bbc936e4dd11fa8337b91a6e0bca9b24
Artikunazo <artik16032011@gmail.com>
2022-06-13 02:41:07 -0700

    2 files modified

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 
 //import 'bootstrap/dist/css/bootstrap.min.css';
-import 'materialize-css/dist/css/materialize.min.css';
+//import 'materialize-css/dist/css/materialize.min.css';
+import 'bulma/css/bulma.css';
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
