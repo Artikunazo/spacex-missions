a9a3957478aa515db2a64d5d331eb36def2b262b
Artikunazo <artik16032011@gmail.com>
2022-06-14 19:55:43 -0700

    1 file modified

--- src/modules/launch-details/LaunchDetails.component.jsx
+++ src/modules/launch-details/LaunchDetails.component.jsx
@@ -2,16 +2,42 @@ import React, { useState, useEffect } from 'react';
 import * as API from '../../services/launches';
 import { useParams } from 'react-router-dom';
 
-export default function LaunchDetails({ flightNumber, ...props }) {
+export default function LaunchDetails() {
   const { launchId } = useParams();
-  const [launchDetails, setLaunchDetails] = useState([]);
+  const [launchDetails, setLaunchDetails] = useState({});
 
   useEffect(() => {
     API.getLaunchByFlight(launchId).then(setLaunchDetails);
-  });
+  }, {});
   return (
     <>
-      <p>{launchDetails[0]}</p>
+      {console.log(launchDetails)}
+      <section className="card">
+        <div className="card-content">
+          <div className="media-content">
+            <h2 class="title is-3 is-flex is-justify-content-space-between">
+              {launchDetails.mission_name}
+              <small>
+                <span
+                  className={` ${
+                    launchDetails.launch_success
+                      ? 'tag is-success'
+                      : 'tag is-danger'
+                  }`}
+                >
+                  {launchDetails.launch_success ? 'Success' : 'Failure'}
+                </span>
+              </small>
+            </h2>
+            <hr />
+          </div>
+
+          <div className="content">
+            <p>Launch year: {launchDetails.launch_year}</p>
+            <p>{launchDetails.details}</p>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
