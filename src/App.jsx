b64a1a1b6afc1b1be38f69fb293fc46f140925e6
Artikunazo <artik16032011@gmail.com>
2022-06-17 09:55:02 -0700

    1 binaries uploaded

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import './style.css';
 import Routes from './Routes';
+import { Link } from 'react-router-dom';
 
 export default function App() {
   return (
     <>
       <section className="container px-3 py-2">
+        <section className="hero">
+          <div className="hero-body">
+            <p className="title">
+              <Link to="/">
+                <img src="" alt="SpaceX Logo" />
+              </Link>
+            </p>
+            <p className="subtitle"></p>
+          </div>
+        </section>
         <h1 className="title is-1">SpaceX Launches</h1>
         <Routes />
       </section>
